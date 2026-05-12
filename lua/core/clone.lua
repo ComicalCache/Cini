f180eab9c73978782d6baefb53eced3347b05c2e
ComicalCache <57747276+ComicalCache@users.noreply.github.com>
2026-05-12 13:48:10 +0200

    Fix a Lua deepclone bug causing errors in some situations

--- lua/core/clone.lua
+++ lua/core/clone.lua
@@ -9,23 +9,34 @@ end
 --- @param obj any
 --- @return any
 function Clone.deepclone(obj)
+    if obj == nil then return nil end
+
     local copy
 
     if type(obj) == 'table' then
         copy = {}
 
         for obj_key, obj_value in next, obj, nil do
-            copy[Clone.deepclone(obj_key)] = Clone.deepclone(obj_value)
+            local cloned_key = Clone.deepclone(obj_key)
+            if cloned_key ~= nil then
+                copy[cloned_key] = Clone.deepclone(obj_value)
+            end
         end
 
-        setmetatable(copy, Clone.deepclone(getmetatable(obj)))
+        local mt = getmetatable(obj)
+        if mt then setmetatable(copy, Clone.deepclone(mt)) end
     elseif type(obj) == 'userdata' then
         local mt = getmetatable(obj)
 
-        -- C++ defined userdata may offer a clone function to clone C++ bound objects.
-        if mt and type(mt.__index) == 'table' and type(mt.__index.clone) == 'function' then
-            copy = obj:clone()
-        elseif type(obj.clone) == 'function' then
+        local success, clone_func = pcall(function()
+            if mt and type(mt.__index) == 'table' and type(mt.__index.clone) == 'function' then
+                return mt.__index.clone
+            end
+
+            return obj.clone
+        end)
+
+        if success and type(clone_func) == 'function' then
             copy = obj:clone()
         else
             copy = obj
